2e03a7c722a1c79ed80511f7682ea80008e16ee2
Islam <Ihtyyarovich@gmail.com>
2021-10-20 17:19:22 +0300

    added models

---
+++ CommandsService/Models/Platform.cs
@@ -0,0 +1,10 @@
+﻿namespace CommandsService.Models
+{
+    public class Platform
+    {
+        public int Id {  get; set; }
+        public int ExternalID { get; set; }
+        public string Name { get; set; }
+        public ICollection<Command> Commands {  get; set;} = new List<Command>(); 
+    }
+}
