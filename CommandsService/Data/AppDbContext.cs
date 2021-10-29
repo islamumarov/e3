4fa90073c1d09b9a19b5f4bb564963bc73806732
Islam <Ihtyyarovich@gmail.com>
2021-10-29 12:30:53 +0300

    added data layer

---
+++ CommandsService/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+using CommandsService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandsService
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Command> Commands { get; set; }
+        public DbSet<Platform> Platforms { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Platform>()
+                .HasMany(p => p.Commands)
+                .WithOne(c => c.Platform!)
+                .HasForeignKey(c => c.PlatformId);
+            modelBuilder.Entity<Command>()
+                .HasOne(c => c.Platform)
+                .WithMany(p => p.Commands)
+                .HasForeignKey(c => c.PlatformId);
+        }
+    }
+
+}
--- CommandsService/Models/Command.cs
+++ CommandsService/Models/Command.cs
@@ -1,9 +1,15 @@
-﻿namespace CommandsService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Models
 {
     public class Command
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string HowTo { get; set; }
+        [Required]
         public int PlatformId { get; set; }
         public Platform Platform { get; set; }
     }
